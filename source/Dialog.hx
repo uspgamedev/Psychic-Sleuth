418b003dd47f6abc853e095ec2746792b9c15989
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-01-26 10:56:22 -0200

    Reorganizing dialogs (and added a few)

---
+++ source/Dialog.hx
@@ -0,0 +1,112 @@
+
+    class Dialog {
+
+        static public function createAll (dialogs: Array<String>) {
+
+    /* 00 */dialogs.push("This is the crime scene." +
+                         "\n   You are the detective. But not a regular one."); // 0
+    /* 01 */dialogs.push("While in a crime scene, you have the power so see" +
+                         "\n   what happend..." ); // 1
+    /* 02 */dialogs.push("... as you if you were the criminal."); // 2
+    /* 03 */dialogs.push(""); // 3
+    /* 04 */dialogs.push("Hum... What a mystery."); // 4
+    /* 05 */dialogs.push(""); // 5
+    /* 06 */dialogs.push("(crying) I can't belive what happened."); // 6
+    /* 07 */dialogs.push(""); // 7
+    /* 08 */dialogs.push("You are not thinking I've done that, are you?"); // 8
+    /* 09 */dialogs.push(""); // 9
+    /* 10 */dialogs.push("Cause of death: trauma to the back of the head by" +
+                         "\n   a blunt weapon."); // 10
+    /* 11 */dialogs.push(""); // 11
+    /* 12 */dialogs.push("Click on the room where you wish to go."); //12
+    /* 13 */dialogs.push(""); //13
+
+            // Woman
+    /* 14 */dialogs.push("This is terrible... With all the things I've done..." +
+                         "\n   sob... He even hugged me after seeing the body...");
+    /* 15 */dialogs.push("");
+    /* 16 */dialogs.push("Me and Hipster Guy argued about sending Big Guy to rehab." +
+                         "\n   He said it wasn't necessary! Did he not care about my" +
+                         "\n   my husband!?");
+    /* 17 */dialogs.push("");
+    /* 18 */dialogs.push("You know, I love my husband, but he has a problem with" +
+                         "\n   drugs...");
+    /* 19 */dialogs.push("");
+    /* 20 */dialogs.push("I don't know where this key came from!");
+    /* 21 */dialogs.push("");
+    /* 22 */dialogs.push("Yes, we had an affair. Hipster Guy consoled me when my" +
+                         "\n   husband was... altered.");
+    /* 23 */dialogs.push("");
+
+            // Man
+    /* 24 */dialogs.push("My hammer? I lent it to a friend.");
+    /* 25 */dialogs.push("");
+    /* 26 */dialogs.push("Yes, the drugs are mine... By that has nothing to do" +
+                         "\n   with the issue at hand, so shouldn't we focus in the" +
+                         "\n   murder?");
+    /* 27 */dialogs.push("");
+    /* 28 */dialogs.push("I was taking a shower. When I left the bathroom, he was" +
+                         "\n   already laying on the floor. The house was locked with only" +
+                         "\n   my wife and I, until you arrived.");
+    /* 29 */dialogs.push("");
+    /* 30 */dialogs.push("Why the hell was she with the key?! That's very suspecious!");
+    /* 31 */dialogs.push("");
+
+            // Toolbox
+    /* 32 */dialogs.push("What a nice toolbox, hum.");
+    /* 33 */dialogs.push("");
+    /* 34 */dialogs.push("Well, well. It looks like there is no hammer in this" +
+                         "\n   toolbox. Strange, isn't it?");
+    /* 35 */dialogs.push("");
+
+            // Door
+    /* 36 */dialogs.push("This door's lock is reachable from both sides. But there is no" +
+                         "\n   key.");
+    /* 37 */dialogs.push("");
+    /* 38 */dialogs.push("Let's see if the missing key is the one I found earlier." +
+                         "\n   *checks*\n    Yup, that's the one.");
+    /* 39 */dialogs.push("");
+    /* 40 */dialogs.push("This door's lock is reachable from both sides. Hum, the key is" +
+                         "\n   missing, but perhaps... Yup, that key from earlier goes" +
+                         "\n   here.");
+    /* 41 */dialogs.push("");
+
+            // Key
+    /* 42 */dialogs.push("Hum... This must open that door.");
+    /* 43 */dialogs.push("");
+    /* 44 */dialogs.push("Let's find out where I can use it.");
+    /* 45 */dialogs.push("");
+
+            // Newspaper
+    /* 46 */dialogs.push("There is a picture of Big Guy holding a hammer in front of a" +
+                         "\n   huge treehouse. Hum, where can I find that hammer?");
+    /* 47 */dialogs.push("");
+
+            // Hammer
+    /* 48 */dialogs.push("This is the murder weapon! Now I can solve the case." +
+                         "\n   The cuprit is...");
+    /* 49 */dialogs.push("");
+
+            // Lamp
+    /* 50 */dialogs.push("Maybe this could have caused the trauma to the victim's head.");
+    /* 51 */dialogs.push("");
+
+            // Booklets
+    /* 52 */dialogs.push("Here are some flyers about a drug rehab program. Not" +
+                         "\n   suspecious at all.");
+    /* 53 */dialogs.push("");
+
+            // PC
+    /* 54 */dialogs.push("You can find a lot about people from their PCs. This one's" +
+                         "\n   mail history (which I just hacked) reveals that Crybaby" +
+                         "\n   Lady and Hipster Guy were having an affair. Well, now" +
+                         "\n   ain't that a good motive Big Guy-dono.");
+    /* 55 */dialogs.push("");
+
+            // Toilet
+    /* 56 */dialogs.push("Always check out the toilet. Them goodies are always there." +
+                         "\n   Aaaaand... Aha! A pack of marijuana! Now whose are these?");
+    /* 57 */dialogs.push("");
+        }
+
+    }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -86,92 +86,7 @@ class PlayState extends State {
 
     private function createDialogs(): Void {
         dialogs = new Array<String>();
-
-/* 00 */dialogs.push("This is the crime scene." +
-                     "\n   You are the detective. But not a regular one."); // 0
-/* 01 */dialogs.push("While in a crime scene, you have the power so see" +
-                     "\n   what happend..." ); // 1
-/* 02 */dialogs.push("... as you if you were the criminal."); // 2
-/* 03 */dialogs.push(""); // 3
-/* 04 */dialogs.push("Hum... What a mystery."); // 4
-/* 05 */dialogs.push(""); // 5
-/* 06 */dialogs.push("(crying) I can't belive what happened."); // 6
-/* 07 */dialogs.push(""); // 7
-/* 08 */dialogs.push("You are not thinking I've done that, are you?"); // 8
-/* 09 */dialogs.push(""); // 9
-/* 10 */dialogs.push("Cause of death: trauma to the back of the head by" +
-                     "\n   a blunt weapon."); // 10
-/* 11 */dialogs.push(""); // 11
-/* 12 */dialogs.push("Click on the room where you wish to go."); //12
-/* 13 */dialogs.push(""); //13
-
-        // Woman
-/* 14 */dialogs.push("This is terrible... With all the things I've done..." +
-                     "\n   sob... He even hugged me after seeing the body...");
-/* 15 */dialogs.push("");
-/* 16 */dialogs.push("Me and Hipster Guy argued about sending Big Guy to rehab." +
-                     "\n   He said it wasn't necessary! Did he not care about my" +
-                     "\n   my husband!?");
-/* 17 */dialogs.push("");
-/* 18 */dialogs.push("You know, I love my husband, but he has a problem with" +
-                     "\n   drugs...");
-/* 19 */dialogs.push("");
-/* 20 */dialogs.push("I don't know where this key came from!");
-/* 21 */dialogs.push("");
-/* 22 */dialogs.push("Yes, we had an affair. Hipster Guy consoled me when my" +
-                     "\n   husband was... altered.");
-/* 23 */dialogs.push("");
-
-        // Man
-/* 24 */dialogs.push("My hammer? I lent it to a friend.");
-/* 25 */dialogs.push("");
-/* 26 */dialogs.push("Yes, the drugs are mine... By that has nothing to do" +
-                     "\n   with the issue at hand, so shouldn't we focus in the" +
-                     "\n   murder?");
-/* 27 */dialogs.push("");
-/* 28 */dialogs.push("I was taking a shower. When I left the bathroom, he was" +
-                     "\n   already laying on the floor. The house was locked with only" +
-                     "\n   my wife and I, until you arrived.");
-/* 29 */dialogs.push("");
-/* 30 */dialogs.push("Why the hell was she with the key?! That's very suspecious!");
-/* 31 */dialogs.push("");
-
-        // Toolbox
-/* 32 */dialogs.push("What a nice toolbox, hum.");
-/* 33 */dialogs.push("");
-/* 34 */dialogs.push("Well, well. It looks like there is no hammer in this" +
-                     "\n   toolbox. Strange, isn't it?");
-/* 35 */dialogs.push("");
-
-        // Door
-/* 36 */dialogs.push("This door's lock is reachable from both sides. But there is no key.");
-/* 37 */dialogs.push("");
-/* 38 */dialogs.push("Here we go, let's test it. Yup, the missing key is definetly" +
-                     "\n   this one.");
-/* 39 */dialogs.push("");
-/* 40 */dialogs.push("This door's lock is reachable from both sides. Hum, the key is" +
-                     "\n   missing, but perhaps... Yup, that key goes here.");
-/* 41 */dialogs.push("");
-
-        // Key
-/* 42 */dialogs.push("Hum... This must open that door.");
-/* 43 */dialogs.push("");
-/* 44 */dialogs.push("Let's find out where I can use it.");
-/* 45 */dialogs.push("");
-
-        // Newspaper
-/* 46 */dialogs.push("There is a picture of Big Guy holding a hammer in front of a" +
-                     "\n   huge treehouse. Hum, where can I find that hammer?");
-/* 47 */dialogs.push("");
-
-        // Hammer
-/* 48 */dialogs.push("This is the murder weapon! Now I can solve the case." +
-                     "\n   The cuprit is...");
-/* 49 */dialogs.push("");
-
-        // Lamp
-/* 50 */dialogs.push("Maybe this could have caused the trauma to the victim's head.");
-/* 51 */dialogs.push("");
+        Dialog.createAll(dialogs);
     }
 
     private function raiseDialog(timer: FlxTimer): Void {
@@ -334,57 +249,80 @@ class PlayState extends State {
 
     // Move an item to itemBar.
     private function moveToBar(button: Button): Void {
-        // If it is already on the bar, do nothing.
-        if (button.getY() < 40) {
-            return;
-        }
-        if (button == hammer) {
-            //TODO
-        } else if (button == booklets) {
-            hasBooklet = true;
-        } else if (button == key) {
-            hasKey = true;
-            if (noKeyInBathroom) {
-                how = true;
+        var maybeDialogIndex: Int = -1;
+        do {
+            // If it is already on the bar, do nothing.
+            if (button.getY() < 40) {
+                break;
             }
-        } else if (button == lamp) {
-            dialogIndex = 50;
-            raiseDialog(timer);
-            return;
-        } else if (button == door) {
-            noKeyInBathroom = true;
-            if (hasKey) {
-                how = true;
+            if (button == hammer) {
+                //TODO
+            } else if (button == booklets) {
+                maybeDialogIndex = 52;
+                hasBooklet = true;
+            } else if (button == key) {
+                hasKey = true;
+                if (noKeyInBathroom) {
+                    maybeDialogIndex = 42;
+                    how = true;
+                } else {
+                    maybeDialogIndex = 44;
+                }
+            } else if (button == lamp) {
+                maybeDialogIndex = 50;
+                break;
+            } else if (button == door) {
+                if (!noKeyInBathroom && hasKey) {
+                    maybeDialogIndex = 40;
+                    how = true;
+                    break;
+                }
+                noKeyInBathroom = true;
+                maybeDialogIndex = 36;
+                if (hasKey) {
+                    maybeDialogIndex = 38;
+                    how = true;
+                }
+                break;
+            } else if (button == pc) {
+                maybeDialogIndex = 54;
+                knownAffair = true;
+                why = true;
+                break;
+            } else if (button == toilet) {
+                maybeDialogIndex = 56;
+                drugs.revive();
+                hasDrugs = true;
+                button = drugs;
+            } else if (button == toolbox) {
+                if (carpinter) {
+                    maybeDialogIndex = 34;
+                    noHammer = true;
+                } else {
+                    maybeDialogIndex = 32;
+                }
+                break;
+            } else if (button == newspaper) {
+                carpinter = true;
+                maybeDialogIndex = 46;
+                break;
             }
-            return;
-        } else if (button == pc) {
-            knownAffair = true;
-            why = true;
-            return;
-        } else if (button == toilet) {
-            drugs.revive();
-            hasDrugs = true;
-            button = drugs;
-        } else if (button == toolbox) {
-            noHammer = true;
-            return;
-        } else if (button == newspaper) {
-            carpinter = true;
-            dialogIndex = 46;
-            raiseDialog(timer);
-            return;
-        }
-        itemBar.push(button);
+            itemBar.push(button);
 
-        var options: TweenOptions;
-        options = {
-            type: FlxTween.ONESHOT,
-        };
-        // Explanation: linearMotion(object, fromX, fromY, toX, toY,
-        //                           durationOrSpeed, useAsDuration, options)
-        FlxTween.linearMotion(button, button.getX(), button.getY(),
-                              140 + 40 * itemBar.length, 32, 0.65, true,
-                              options);
+            var options: TweenOptions;
+            options = {
+                type: FlxTween.ONESHOT,
+            };
+            // Explanation: linearMotion(object, fromX, fromY, toX, toY,
+            //                           durationOrSpeed, useAsDuration, options)
+            FlxTween.linearMotion(button, button.getX(), button.getY(),
+                                  140 + 40 * itemBar.length, 32, 0.65, true,
+                                  options);
+        } while (false);
+        if (maybeDialogIndex >= 0) {
+            dialogIndex = maybeDialogIndex;
+            raiseDialog(timer);
+        }
     }
 
     private function roomCallback(button: Button): Void {
